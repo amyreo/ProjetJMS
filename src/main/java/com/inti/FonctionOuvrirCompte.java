1dd106678bd62af7e7c201caac407f7df07da72e
FlorentEstur <warcr@DESKTOP-3VUL9G4>
2022-12-02 10:33:12 +0100

    Test branche Florent + début fonction ouvrir compte

---
+++ src/main/java/com/inti/FonctionOuvrirCompte.java
@@ -0,0 +1,19 @@
+package com.inti;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.inti.service.ICompteRepository;
+
+public class FonctionOuvrirCompte {
+
+	@Autowired
+	ICompteRepository icr;
+	
+	public void ouvrirCompte () {
+		
+		
+		
+		
+	}
+	
+}
