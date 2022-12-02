397acb924438626ab71c71c8f1d735d15e65b941
theolorenzi <theo.lorenzi@laposte.net>
2022-12-02 09:45:59 +0100

    Premier commit

---
+++ src/main/java/com/inti/ProjetJmsApplication.java
@@ -0,0 +1,13 @@
+package com.inti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetJmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetJmsApplication.class, args);
+	}
+
+}
